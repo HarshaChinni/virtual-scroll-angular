f89f8b7a73e285d45c710935cc25584d368e5e6d
Sri Harsha Chinni <sriharshachinni@Harshas-MacBook-Pro.local>
2021-02-09 19:32:25 -0700

    Setting up the Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mat-select-virtual-drop-down';
+  zipCodeList = [];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  userForm = new FormGroup({
+    firstName: new FormControl('Harsha'),
+    lastName: new FormControl('Chinni'),
+    companyName: new FormControl('Galileo Financial Technologies'),
+    currentRole: new FormControl('Full stack developer'),
+    address: new FormGroup({
+      city: new FormControl(''),
+      country: new FormControl('United States'),
+      state: new FormControl(''),
+      zipCode: new FormControl(this.zipCodeList)
+    })
+  });
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
